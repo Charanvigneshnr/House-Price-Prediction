e2ed4bf5ea5fc4ed563fe78d64d553859e8a8c5a
Charanvigneshnr <charanvignesh10@gmail.com>
2023-10-07 22:32:02 +0530

    Stable Local Build

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ import requests
 import streamlit as st
 import pandas as pd
 
-
 def main():
-    st.set_page_config(page_title="Bangalore Price Prediction App", page_icon=":house:")
+    st.set_page_config(page_title="Bangalore House Price Prediction App", page_icon=":house:")
 
-    # Navigation bar
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", ["Home", "About"])
 
@@ -17,10 +15,10 @@ def main():
 
 
 def show_home():
-    st.title("Bangalore Price Prediction App")
+    st.title("Bangalore House Price Prediction App")
 
     total_sqft = st.number_input("Total Square Feet", min_value=300, max_value=10000, value=1500)
-    location = st.text_input("Location")
+    location = st.text_input("Location", value="Indira Nagar")
     bhk = st.number_input("BHK", min_value=1, max_value=5, value=3, step=1, format="%d")
     bath = st.number_input("Number of Bathrooms", min_value=1, max_value=5, value=2, step=1, format="%d")
 
@@ -28,6 +26,15 @@ def show_home():
         estimated_price = predict_price(location, total_sqft, bhk, bath)
         estimated_price_display = format_price(abs(estimated_price))
         st.success(f"Estimated Price: {estimated_price_display}")
+
+    st.markdown("## Instructions to Run the App")
+    st.markdown("1. To run this app, download these files in your PC: [Download Files]("
+                "https://drive.google.com/drive/folders/1frXr2-VZ9_HFOkIyuG1MRHMte6AmlzTl?usp=sharing)")
+
+    st.markdown("2. Open the terminal and navigate to the downloaded folder.")
+
+    st.markdown("3. Run the following command in the terminal:")
+    st.code("python serv.py", language="python")
 
 
 def format_price(price):
@@ -38,7 +45,7 @@ def format_price(price):
 
 
 def show_about():
-    st.title("About Bangalore Price Prediction App")
+    st.title("About Bangalore House Price Prediction App")
 
     st.write(
         "Welcome to the Bangalore Price Prediction App! This app uses a Linear Regression model to predict house "
@@ -53,7 +60,7 @@ def show_about():
     st.subheader("Model Comparison")
     comparison_data = {
         'Model': ['Linear Regression', 'Lasso', 'Decision Tree'],
-        'Best Score': [0.819001, 0.687464, 0.715887],
+        'Best Score': ["81.90%", "68.74%", "71.58%"],
     }
     st.write(pd.DataFrame(comparison_data).set_index('Model'))
 
